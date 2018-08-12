36feffa2657f152aebbfc00f4e7d73d3ef706715
Alex Jobe <ajobe15@gmail.com>
2018-08-11 23:35:44 -0500

    Add collision detection between bullets and enemies, and create enemy death sequence

---
+++ Assets/Scripts/SelfDestructor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructor : MonoBehaviour {
+
+    [SerializeField] float timeUntilDestroyed = 5f;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, timeUntilDestroyed);
+	}
+}
