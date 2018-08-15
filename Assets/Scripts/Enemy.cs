735fa00772770ee098836827d86c6a3e8f8837f2
Alex Jobe <ajobe15@gmail.com>
2018-08-14 22:11:01 -0500

    Add scoring system

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,15 +7,26 @@ public class Enemy : MonoBehaviour {
 
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform fxParent;
+    [SerializeField] int scorePerHit = 10;
+
+    ScoreBoard scoreBoard;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
+    {
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
     }
 
     private void OnParticleCollision(GameObject other)
     {
+        scoreBoard.ScoreHit(scorePerHit);
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = fxParent;
         Destroy(gameObject);
